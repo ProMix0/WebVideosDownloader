22eba0477caef850581da87a937fd53e72a1995f
ProMix0 <misha.zavertkin@mail.ru>
2022-05-13 17:33:21 +0700

    Добавьте файлы проекта.

---
+++ WebViseosDownloader/HtmlParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebVideosDownloader
+{
+    public class HtmlParser
+    {
+        public static async Task<IEnumerable<Match>> Parse(string url, Regex regex)
+        {
+            HttpClient client = new();
+            string html = await client.GetStringAsync(url);
+
+            return regex.Matches(html);
+        }
+
+        public static Task<IEnumerable<Match>> Parse(string url, string pattern) =>
+            Parse(url, new Regex(pattern));
+    }
+}
