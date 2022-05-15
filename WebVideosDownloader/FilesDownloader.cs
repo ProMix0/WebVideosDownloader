6523e47895285c525e8074de35f6b9af28247ab9
ProMix0 <misha.zavertkin@mail.ru>
2022-05-15 20:54:03 +0700

    Fix typo

---
+++ WebVideosDownloader/FilesDownloader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebVideosDownloader
+{
+    public class FilesDownloader
+    {
+        public DirectoryInfo? Directory { get; set; } = null;
+        public Func<string, string> UriToFileName { get; set; } = url => url.Split('/').Last();
+
+        public async Task DownloadFiles(IEnumerable<string> uris, int usedThreads = 3)
+        {
+            await Parallel.ForEachAsync(uris,
+                            new ParallelOptions() { MaxDegreeOfParallelism = usedThreads },
+                            (uri, token) => DownloadFile(uri, UriToFileName(uri), token));
+        }
+
+        public async ValueTask DownloadFile(string url, string fileName, CancellationToken token)
+        {
+            using HttpClient client = new();
+
+            using Stream stream = await client.GetStreamAsync(url, token);
+
+            if (!Directory!.Exists) Directory.Create();
+
+            using Stream fs = File.Create($"{Directory.FullName}/{fileName}");
+            await stream.CopyToAsync(fs, token);
+        }
+    }
+}
---
+++ WebVideosDownloader/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Text.RegularExpressions;
+using WebVideosDownloader;
+
+Console.WriteLine("Hello, World!");
+
+FilesDownloader downloader = new();
+
+foreach (var item in new DownloadInfo[]
+{
+    new(){
+        Path=@"https://sviyash-center.ru/videoteka/videopraktikumy-proshcheniya/effektivnoe-proshchenie/",
+        Directory="E:/Видео/Эффективное прощение"
+    }
+})
+{
+    downloader.Directory = new(item.Directory);
+    await downloader.DownloadFiles((await HtmlParser.Parse(item.Path,
+    @"'([a-zA-Z_\/:\.0-9\%]*\.mp4)'")).Select(match => match.Groups[1].Value));
+}
+
+Console.WriteLine("Completed");
+
+class DownloadInfo
+{
+    public string Path, Directory;
+}
